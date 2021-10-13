246dc2653805cb280257f75643c5fbd95e624adf
estyvalankomah <abenammra2002@gmail.com>
2021-10-13 11:21:24 +0000

    Added .gitignore file and database

--- app.py
+++ app.py
@@ -3,12 +3,43 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
-
+#Init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({ 'msg': 'Hello world!' })
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Init db
+db = SQLAlchemy(app)
+
+#Init ma
+ma = Marshmallow(app)
+
+#Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
+#Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'qty')
+
+#Init schema
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
